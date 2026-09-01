6ac76fc15e37fca5e8077a4351ed1f584031686f
Minh08012005 <minhhh119962@gmail.com>
2026-09-02 05:54:21 +0700

    feat: implement getHumanChoice with input

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let input = prompt("Do you choose rock, paper or scissors?");
+    input = input.toLowerCase();
+    if (input === "rock" || input === "paper" || input === "scissors") {
+        return input;
+    } else {
+        alert("Invalid choice! Please choose rock, paper, or scissors.");
+        return getHumanChoice(); // Prompt again for valid input
+    }
+}
+console.log(getHumanChoice());
